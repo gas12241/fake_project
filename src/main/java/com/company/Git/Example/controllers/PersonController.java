dbc52bb0659d77336b05ed78f1aad97592c59460
gas12241 <georgepitcher11@gmail.com>
2023-06-28 18:18:20 -0700

    added work to the person branch

--- src/main/java/com/company/Git/Example/controllers/PersonController.java
+++ src/main/java/com/company/Git/Example/controllers/PersonController.java
@@ -1,4 +1,30 @@
 package com.company.Git.Example.controllers;
 
+import com.company.Git.Example.models.Person;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+import java.util.Random;
+
 public class PersonController {
+
+    private Person[] people = {
+            new Person("name", 14, BigDecimal.valueOf(175.45)),
+            new Person("Shaq", 52, BigDecimal.valueOf(242.22)),
+            new Person("Kobe", 14, BigDecimal.valueOf(222.45)),
+            new Person("Steph", 14, BigDecimal.valueOf(200.72)),
+    };
+
+    @RequestMapping(value = "/person", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public Person getPerson() {
+
+        Random rand = new Random();
+
+        return people[rand.nextInt(4)];
+    }
 }
--- src/main/java/com/company/Git/Example/models/Person.java
+++ src/main/java/com/company/Git/Example/models/Person.java
@@ -1,4 +1,64 @@
 package com.company.Git.Example.models;
 
+import com.company.Git.Example.controllers.PersonController;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
 public class Person {
+    private String name;
+    private Integer age;
+    private BigDecimal heightInCM;
+
+    public Person(String name, Integer age, BigDecimal heightInCM) {
+        this.name = name;
+        this.age = age;
+        this.heightInCM = heightInCM;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public BigDecimal getHeightInCM() {
+        return heightInCM;
+    }
+
+    public void setHeightInCM(BigDecimal heightInCM) {
+        this.heightInCM = heightInCM;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(getName(), person.getName()) && Objects.equals(getAge(), person.getAge()) && Objects.equals(getHeightInCM(), person.getHeightInCM());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getAge(), getHeightInCM());
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", heightInCM=" + heightInCM +
+                '}';
+    }
 }
